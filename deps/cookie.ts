fdcd218ab49940e50e08e88f065e94e9bd5474ea
joye <zhoujing2@chelun.com>
2022-02-22 16:55:14 +0800

    update

--- deps/cookie.ts
+++ deps/cookie.ts
@@ -1,5 +1,22 @@
 import { Cookie } from "./post";
 
+interface CookieStoreItem extends Cookie {
+  setTime: number;
+}
+interface CookieStore {
+  [key: string]: Array<CookieStoreItem>;
+}
+
+const cookieCache: CookieStore = {};
+
+// b.baidu.com
+
+// a.b.baidu.com
+
+// baidu.com
+// b.baidu.com
+// a.b.baidu.com
+
 /**
  * 接收到服务端下发的cookie
  * @param cookies
@@ -10,6 +27,64 @@ export function handleReceivedCookies(cookies?: Cookie[]) {
 
 export function getWillSendCookies(url: string): string {
   let output = "";
+  const urlObj = new URL(url);
+
+  // domain匹配
+  const parts = urlObj.hostname.split(".");
+  const possibleDomains: string[] = [];
+  let tmpDomain = parts[parts.length - 1];
+  for (let i = parts.length - 2; i >= 0; i--) {
+    tmpDomain = parts[i] + "." + tmpDomain;
+    possibleDomains.push(tmpDomain);
+  }
+  let cookies: Array<CookieStoreItem> | undefined = undefined;
+  for (let i = 0; i < possibleDomains.length; i++) {
+    if (cookieCache[possibleDomains[i]]) {
+      cookies = cookieCache[possibleDomains[i]];
+      break;
+    }
+  }
+
+  // cookie不存在，直接返回空
+  if (!cookies || cookies.length == 0) {
+    return output;
+  }
+
+  const expireIndex: number[] = [];
+  const readList: number[] = [];
+  const now = Date.now() / 1000;
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    if (urlObj.pathname.startsWith(cookie.path)) {
+      // 判断是否过期 maxAge
+      if (typeof cookie.maxAge === "number") {
+        // 如果cookie过期
+        if (cookie.setTime + cookie.maxAge < now) {
+          expireIndex.push(i);
+        } else {
+          readList.push(i);
+        }
+      }
+      // 判断是否过期 maxAge
+      else if (cookie.expires) {
+        const expireTime = Date.parse(cookie.expires) / 1000;
+        if (now > expireTime) {
+          expireIndex.push(i);
+        } else {
+          readList.push(i);
+        }
+      } 
+      // 如果maxAge和expire都不存在，是sessionCookie
+      else {
+        expireIndex.push(i);
+      }
+    }
+  }
+
+  // 
+
+  
+
   //1、判断domain匹配
   //2、判断path是否匹配
   //3、判断过期
--- dist/cookie.js
+++ dist/cookie.js
@@ -1,9 +1,56 @@
+const cookieCache = {};
 export function handleReceivedCookies(cookies) {
     if (!Array.isArray(cookies) || cookies.length === 0)
         return;
 }
 export function getWillSendCookies(url) {
     let output = "";
+    const urlObj = new URL(url);
+    const parts = urlObj.hostname.split(".");
+    const possibleDomains = [];
+    let tmpDomain = parts[parts.length - 1];
+    for (let i = parts.length - 2; i >= 0; i--) {
+        tmpDomain = parts[i] + "." + tmpDomain;
+        possibleDomains.push(tmpDomain);
+    }
+    let cookies = undefined;
+    for (let i = 0; i < possibleDomains.length; i++) {
+        if (cookieCache[possibleDomains[i]]) {
+            cookies = cookieCache[possibleDomains[i]];
+            break;
+        }
+    }
+    if (!cookies || cookies.length == 0) {
+        return output;
+    }
+    const expireIndex = [];
+    const readList = [];
+    const now = Date.now() / 1000;
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        if (urlObj.pathname.startsWith(cookie.path)) {
+            if (typeof cookie.maxAge === "number") {
+                if (cookie.setTime + cookie.maxAge < now) {
+                    expireIndex.push(i);
+                }
+                else {
+                    readList.push(i);
+                }
+            }
+            else if (cookie.expires) {
+                const expireTime = Date.parse(cookie.expires) / 1000;
+                if (now > expireTime) {
+                    expireIndex.push(i);
+                }
+                else {
+                    readList.push(i);
+                }
+            }
+            else {
+                expireIndex.push(i);
+            }
+        }
+    }
     return "a=value; b=value";
     return "";
 }
